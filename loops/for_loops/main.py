8c23e806f8f9bbd8a91988bb61659ad73f6e4b99
rhinnant <>
2026-03-22 02:16:48 +0000

    For Loops

---
+++ loops/for_loops/main.py
@@ -0,0 +1,19 @@
+prices = [12.99, 8.50, 15.75, 23.00, 7.25]
+#total = 'total' + prices
+
+# Write your code here
+total = 0
+
+for price in prices:
+    print(price)
+    total += price
+    
+print("Total price for products:", total)
+
+book_name = ["wing", "worm", "beast man"]
+for name in book_name:
+    print(name)
+
+dog = "wolf"
+for dog_type in dog:
+    print(dog_type)
